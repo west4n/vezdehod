838f4c563af2c8066f21aa456cdf366deeb50800
Ivan Kurski <iskurskiii@gmail.com>
2021-08-16 00:04:41 +0300

    added files

---
+++ app/js/app.js
@@ -0,0 +1,26 @@
+import {Swiper, Parallax, Mousewheel, Controller} from 'swiper';
+Swiper.use([ Parallax, Mousewheel, Controller ]);
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	const swiperIMG = new Swiper('.slider-img', {
+
+		loop: false,
+		speed: 2400,
+		parallax: true
+
+	});
+
+	const swiperText = new Swiper('.slider-text', {
+
+		loop: false,
+		speed: 2400,
+		mousewheel: {
+			invert: false
+		}
+	});
+
+	swiperIMG.controller.control = swiperText;
+	swiperText.controller.control = swiperIMG;
+
+});

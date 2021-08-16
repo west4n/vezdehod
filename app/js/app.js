bb81b33633a4a58b6e7083de0d658d981cfa83bf
west4n <iskurskiii@gmail.com>
2021-08-16 17:40:51 +0300

    added new files

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,7 @@
-import {Swiper, Parallax, Mousewheel, Controller} from 'swiper';
-Swiper.use([ Parallax, Mousewheel, Controller ]);
+import { Swiper, Parallax, Mousewheel, Controller, Pagination, Scrollbar, Navigation } from 'swiper';
+Swiper.use([Parallax, Mousewheel, Controller, Pagination, Scrollbar, Navigation]);
+
+import { gsap, Power2 } from 'gsap';
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -17,10 +19,38 @@ document.addEventListener('DOMContentLoaded', () => {
 		speed: 2400,
 		mousewheel: {
 			invert: false
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true
+		},
+		scrollbar: {
+			el: '.swiper-scrollbar',
+			draggable: true
+		},
+		navigation: {
+			prevEl: '.swiper-button-prev',
+			nextEl: '.swiper-button-next'
 		}
 	});
 
 	swiperIMG.controller.control = swiperText;
 	swiperText.controller.control = swiperIMG;
+
+	let gear = document.querySelector('.slider-gear');
+
+	swiperText.on('slideNextTransitionStart', () => {
+		gsap.to(gear, 2.8, {
+			rotation: '+=40',
+			ease: Power2.easeOut
+		})
+	})
+
+	swiperText.on('slidePrevTransitionStart', () => {
+		gsap.to(gear, 2.8, {
+			rotation: '-=40',
+			ease: Power2.easeOut
+		})
+	})
 
 });

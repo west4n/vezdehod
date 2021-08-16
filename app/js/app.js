df8e6eba75b9c60c05437d81dd6fb7657b3c2790
Ivan Kurski <iskurskiii@gmail.com>
2021-08-16 23:18:20 +0300

    added new files

--- app/js/app.js
+++ app/js/app.js
@@ -3,13 +3,32 @@ Swiper.use([Parallax, Mousewheel, Controller, Pagination, Scrollbar, Navigation]
 
 import { gsap, Power2 } from 'gsap';
 
+import Micromodal from 'micromodal';
+
 document.addEventListener('DOMContentLoaded', () => {
+
+	Micromodal.init({
+		openTrigger: 'data-micromodal-open',
+		closeTrigger: 'data-micromodal-close',
+		disableFocus: true,
+		disableScroll: true,
+		awaitCloseAnimation: true,
+		awaitOpenAnimation: true,
+	});
 
 	const swiperIMG = new Swiper('.slider-img', {
 
 		loop: false,
 		speed: 2400,
-		parallax: true
+		parallax: true,
+		pagination: {
+			el: '.slider-pagination-count .total',
+			type: 'custom',
+			renderCustom: (swiper, current, total) => {
+				let totalRes = total >= 10 ? total : `0${total}`;
+				return totalRes;
+			}
+		}
 
 	});
 
@@ -52,5 +71,37 @@ document.addEventListener('DOMContentLoaded', () => {
 			ease: Power2.easeOut
 		})
 	})
+
+	let curnum = document.querySelector('.slider-pagination-count .current'),
+			pagecur = document.querySelector('.slider-pagination-current__num');
+
+	swiperText.on('slideChange', () => {
+		let index = swiperText.realIndex + 1;
+		let indRes = index >= 10 ? index : `0${index}`;
+
+		gsap.to(curnum, .2, {
+			force3D: true,
+			y: -10,
+			opacity: 0,
+			ease: Power2.easeOut,
+			onComplete: () => {
+				gsap.to(curnum, .1, {
+					force3D: true,
+					y: 10
+				});
+				curnum.innerHTML = indRes;
+				pagecur.innerHTML = indRes;
+
+			}
+		});
+		
+		gsap.to(curnum, .2, {
+			force3D: true,
+			y: 0,
+			opacity: 1,
+			ease: Power2.easeOut,
+			delay: .3
+		});
+	});
 
 });

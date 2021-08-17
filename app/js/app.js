32907905aa0f2e49637eaa747c41ba7dd339cc8b
west4n <iskurskiii@gmail.com>
2021-08-17 14:23:38 +0300

    site complete

--- app/js/app.js
+++ app/js/app.js
@@ -73,7 +73,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	})
 
 	let curnum = document.querySelector('.slider-pagination-count .current'),
-			pagecur = document.querySelector('.slider-pagination-current__num');
+		pagecur = document.querySelector('.slider-pagination-current__num');
 
 	swiperText.on('slideChange', () => {
 		let index = swiperText.realIndex + 1;
@@ -94,7 +94,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 			}
 		});
-		
+
 		gsap.to(curnum, .2, {
 			force3D: true,
 			y: 0,
@@ -103,5 +103,53 @@ document.addEventListener('DOMContentLoaded', () => {
 			delay: .3
 		});
 	});
+
+	//CURSOR
+
+	const body = document.querySelector('body');
+	const cursor = document.getElementById('cursor');
+	const links = document.getElementsByTagName('a');
+	let mouseX = 0, mouseY = 0, posX = 0, posY = 0;
+
+	function mouseCoords(e) {
+		mouseX = e.pageX;
+		mouseY = e.pageY;
+	}
+
+	gsap.to({}, .01, {
+		repeat: -1,
+		onRepeat: () => {
+			posX += (mouseX - posX) / 6;
+			posY += (mouseY - posY) / 6;
+			gsap.set(cursor, {
+				css: {
+					left: posX,
+					top: posY
+				}
+			})
+		}
+	})
+
+	for (let i = 0; i < links.length; i++) {
+		links[i].addEventListener('mouseover', () => {
+			cursor.classList.add('active');
+		})
+		links[i].addEventListener('mouseout', () => {
+			cursor.classList.remove('active');
+		})
+	}
+
+	body.addEventListener('mousemove', e => {
+
+		mouseCoords(e)
+		cursor.classList.remove('hidden')
+
+
+	})
+	body.addEventListener('mouseout', e => {
+
+		cursor.classList.add('hidden')
+
+	})
 
 });
